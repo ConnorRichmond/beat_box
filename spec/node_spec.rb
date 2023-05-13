55313d1395d33e4de7f62a63467836cbf2535edd
Connor Richmond <richmond.connor@gmail.com>
2023-05-13 19:15:09 -0400

    CHange double quotes to single

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,11 +1,13 @@
 require './lib/node'
 
 describe Node do
-    it "exists as readable" do
-      node = Node.new("plop")
+
+    it 'exists as readable' do
+      node = Node.new('plop')
       
       expect(node).to be_an_instance_of(Node)
-      expect(node.data).to eq("plop")
+      expect(node.data).to eq('plop')
       expect(node.next_node).to eq(nil)
     end
+
 end
