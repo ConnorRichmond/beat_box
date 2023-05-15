6bac98185f2039138afb69bedf27604aac28bb84
Connor Richmond <richmond.connor@gmail.com>
2023-05-15 17:01:40 -0400

    Add prepend and all methods

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,14 +1,23 @@
 class BeatBox
 
+    valid_beats = ['tee', 'dee', 'deep', 'bop', 'boop', 'la', 'na'].freeze
+
     attr_reader :list
 
-    def initialize
-        @list = LinkedList.new
+    def initialize(list = LinkedList.new)
+        @list = list
     end
 
     def append(data)
         data.split.each do |word|
           @list.append(word)
+        end
+        data
+    end
+
+    def prepend(data)
+        data.split.each do |word|
+            @list.prepend(word)
         end
         data
     end
@@ -20,5 +29,10 @@ class BeatBox
     def play
         `say -r 200 -v nathan #{@list.to_string}`
     end
+
+    def all
+        @list.to_string
+    end
+
 
 end
