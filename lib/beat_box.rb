835d435ad63c1ff3424ce29cf9bbaf00beba78cc
Connor Richmond <richmond.connor@gmail.com>
2023-05-15 18:00:51 -0400

    Change beats_list to method, update append/prepend

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,23 +1,25 @@
 class BeatBox
 
-    beats_list = ['tee', 'dee', 'deep', 'bop', 'boop', 'la', 'na', 'ditt', 'doo', 'shu', 'woo', 'hoo']
-
     attr_reader :list
 
     def initialize(list = LinkedList.new)
         @list = list
     end
 
+    def beats_list
+        ['tee', 'dee', 'deep', 'bop', 'boop', 'la', 'na', 'ditt', 'doo', 'shu', 'woo', 'hoo', 'da', 'do']
+    end
+
     def append(data)
         data.split.each do |word|
-          @list.append(word)
+          @list.append(word) if beats_list.include?(word)
         end
         data
     end
 
     def prepend(data)
         data.split.each do |word|
-            @list.prepend(word)
+            @list.prepend(word) if beats_list.include?(word)
         end
         data
     end
